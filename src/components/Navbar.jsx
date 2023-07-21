7b68fd0375ad8cdbe9463730d590d79a4351a6ef
Toyash <toyashpatil17@gmail.com>
2023-07-21 20:30:19 +0530

    Router Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import Logo from "../assets/Logo.png"
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='h-14 flex items-center  w-full md:p-2'>
-        <div className='p-5 flex gap-4'>
+        <div className='p-5 flex gap-6'>
             <div className='flex h-10 w-10'>
-            <img src={Logo} className='h-10 w-10 rounded-full'></img>
+            <NavLink to='/home'><img src={Logo} className='h-10 w-10 rounded-full'></img></NavLink>
             </div>
-            <div className='flex gap-4 items-center md:text-xl'>
-            <h1>Home</h1>
-            <h1>About</h1>
-            <h1>Tech</h1>
-            <h1>Bussiness</h1>
-            <h1>Sports</h1>
+            <div className='flex gap-6 items-center md:text-xl'>
+            <NavLink to="/tech"><h1>Tech</h1></NavLink>
+            <NavLink to="/bussiness"><h1>Bussiness</h1></NavLink>
+            <NavLink to="/sports"><h1>Sports</h1></NavLink>
+            <NavLink to="/about"><h1>About</h1></NavLink>
             </div>   
         </div>
         <div className='flex gap-4 w-full ml-4 mr-24 justify-end md:mr-16'>
