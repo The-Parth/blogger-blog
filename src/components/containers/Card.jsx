4d68fe33799c13e6c8839bc051dd231f8be7b049
atharvaupare <atharvaupare5@gmail.com>
2023-07-21 21:34:59 +0530

    Card update

--- src/components/containers/Card.jsx
+++ src/components/containers/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({ Url, content, alt, title }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg ml-4 mt-4">
+    <div className="max-w-sm rounded overflow-hidden shadow-[0_35px_60px_-15px_rgba(255,255,255,0.3)] m-4">
       <img className="w-full" src={Url} alt={alt} />
 
       <div class="px-6 py-4  bg-[#031130]">
