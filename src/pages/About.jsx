3244f690ce33a03e5d6e7161d34491ca590789e3
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2023-07-21 21:59:22 +0530

    Added About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
 
-const About = () => {
-  return (
-    <div>
-        <h1>About</h1>
-    </div>
-  )
-}
+const AboutPage = () => {
+    return (
+        <div className="flex justify-center items-start h-screen">
+            <div className="bg-[#343432] p-8 rounded-lg shadow-lg w-full sm:w-[545px] mt-8 mx-10">
+                <h2 className="text-2xl font-bold mb-4">About Us</h2>
+                <p>A simple blog site made using React.js and Tailwind CSS.</p>
 
-export default About
+                <h2 className="text-2xl font-bold mb-4 mt-8">Features Implemented</h2>
+                <ol className="list-decimal pl-5">
+                    <li>Home Page with hero</li>
+                    <li>Blog Page with cards</li>
+                    <li>React Router</li>
+                    <li>Responsive Design</li>
+                    <li>Dark Mode</li>
+                </ol>
+                <h2 className="text-2xl font-bold mb-4 mt-8">The Developers</h2>
+                <ul className="list-disc pl-5">
+                    <li>
+                        <a>
+                            Aastha Deshwal
+                        </a>
+                        <a href = "https://github.com/AasthaDeshwal" target="_blank" className="fa-brands fa-github pl-2 transition-colors duration-300 hover:text-[#4078c0]"></a>
+                    </li>
+                    <li>
+                        <a>
+                            Atharva Upare
+                        </a>
+                        <a href = "https://github.com/atharvaupare" target="_blank" className="fa-brands fa-github pl-2 transition-colors duration-300 hover:text-[#4078c0]"></a>
+                    </li>
+                    <li>
+                        <a>
+                            Parth Bhanushali
+                        </a>
+                        <a href = "https://github.com/The-Parth" target="_blank" className="fa-brands fa-github pl-2 transition-colors duration-300 hover:text-[#4078c0]"></a>
+                        <a href = "https://www.linkedin.com/in/parth-bhanushali-b771ab251/" target="_blank" className="fa-brands fa-linkedin pl-2 transition-colors duration-300 hover:text-[#0077b5]"></a>
+                    </li>
+                    <li>
+                        <a>
+                            Toyash Patil
+                        </a>
+                        <a href = "https://github.com/ToyashPatil" target="_blank" className="fa-brands fa-github pl-2 transition-colors duration-300 hover:text-[#4078c0]"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default AboutPage;
