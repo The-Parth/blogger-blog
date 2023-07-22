bf321203cdadcaed23a0c1490807e144131c9aff
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2023-07-22 17:44:38 +0530

    Format

--- src/components/containers/Card.jsx
+++ src/components/containers/Card.jsx
@@ -1,28 +1,25 @@
-import React,{useContext} from "react"
+import React, { useContext } from "react";
 import Carditem from "./Carditem";
 import BlogContext from "../../context/blog/blogcontext";
 
-
-
 const Card = () => {
-
-  const {blog}=useContext(BlogContext);
-  return (
-    <div className="flex justify-center">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 align-middle">
-      {blog.map((n)=>{
-        return(
-          <div>
-            <Carditem 
-              Url="https://images.theconversation.com/files/182925/original/file-20170822-30538-gebk45.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop"
-              title={n.title}
-              content={n.post}
-            />
-          </div>
-        )
-      })}
-    </div>
-    </div>
-  );
+    const { blog } = useContext(BlogContext);
+    return (
+        <div className="flex justify-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 align-middle">
+                {blog.map((n) => {
+                    return (
+                        <div>
+                            <Carditem
+                                Url="https://images.theconversation.com/files/182925/original/file-20170822-30538-gebk45.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop"
+                                title={n.title}
+                                content={n.post}
+                            />
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 export default Card;
