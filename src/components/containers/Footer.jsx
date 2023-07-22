758246da5d479b0cbe5dff560cb7d3debde52a90
atharvaupare <atharvaupare5@gmail.com>
2023-07-23 04:36:04 +0530

    footer-fixed-gg

--- src/components/containers/Footer.jsx
+++ src/components/containers/Footer.jsx
@@ -3,29 +3,22 @@ import "../../App.css";
 
 export const Footer = () => {
   return (
-    <footer className=" flex shadow bg-[#343432] bottom-0 w-full fixed">
-      <div className="w-full max-w-screen-xl mx-auto  md:py-2  ">
-        <div className="sm:flex sm:items-center sm:justify-center">
+    <footer className="flex shadow bg-[#343432] bottom-0 w-full fixed">
+      <div className="w-full max-w-screen-xl mx-auto px-4 md:px-6 lg:px-8 py-2 sm:py-4 md:py-2">
+        <div className="sm:flex sm:items-center sm:justify-center text-center">
           <a
             href="https://www.linkedin.com/company/csi---kjsce-student's-chapter/"
-            className="flex items-center mb-4 sm:mb-0"
+            className="flex items-center mb-2 sm:mb-0"
           >
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
               Made with
-              <i class="fa-solid fa-heart fa-lg text-red-600 mx-1"></i>
+              <i className="fa-solid fa-heart fa-lg text-red-600 mx-1"></i>
               by Tech Team, CSI-KJSCE.
             </span>
           </a>
-          <figure className=" mix-blend-multiply ">
-            <img
-              src={require("../../assets/Csi-logo.png")}
-              alt="CSI-KJSCE logo "
-              className="h-12 mx-3 brightness-0 invert-0 "
-            />
-          </figure>
         </div>
-        <hr className="my-1 border-gray-200 sm:mx-auto dark:border-gray-700 " />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <hr className="my-1 border-gray-200 sm:mx-auto dark:border-gray-700" />
+        <span className="block text-xs text-gray-500 sm:text-center dark:text-gray-400">
           © 2023 . No Rights Reserved.
         </span>
       </div>
