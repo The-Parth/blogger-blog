cea710bdb26657d13252b0ca41ae1683d9387a7a
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2023-07-22 18:29:54 +0530

    Urgent bug fix

--- src/components/containers/Carditem.jsx
+++ src/components/containers/Carditem.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Carditem = ({ Url, content, alt, title }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-[0_35px_60px_-15px_rgba(255,255,255,0.3)] m-4 h-[400px] ">
+    <div className="max-w-sm rounded overflow-hidden shadow-[0_35px_60px_-15px_rgba(255,255,255,0.3)] m-4 ">
       <img className="w-full" src={Url} alt={alt} />
 
       <div class="px-6 py-4h-10  bg-[#031130] h-[100%]">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-[#7ea4f7] text-base  ">{content}</p>
+        <div className="font-bold text-xl mb-2 pt-4">{title}</div>
+        <p className="text-[#7ea4f7] text-base pb-4 ">{content}</p>
       </div>
     </div>
   );
