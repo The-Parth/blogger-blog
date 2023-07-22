c121f4d2254e5381c7b5fd5464deeb67ef01533b
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2023-07-22 18:24:24 +0530

    Navbar and card height

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,67 @@
-import React from 'react'
-import Logo from "../assets/Logo.png"
-import { NavLink } from 'react-router-dom'
-import Login from './Login'
-import Signup from './Signup'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../assets/Logo.png";
 
 const Navbar = () => {
-  return (
-    <div className='h-14 flex items-center  w-full md:p-2'>
-        <div className='p-5 flex gap-6'>
-            <div className='flex h-10 w-10'>
-            <NavLink to='/home'><img src={Logo} className='h-10 w-10 rounded-full'></img></NavLink>
-            </div>
-            <div className='flex gap-6 items-center md:text-xl'>
-            <NavLink to="/tech"><h1>Tech</h1></NavLink>
-            <NavLink to="/bussiness"><h1>Bussiness</h1></NavLink>
-            <NavLink to="/sports"><h1>Sports</h1></NavLink>
-            <NavLink to="/about"><h1>About</h1></NavLink>
-            </div>   
-        </div>
-        <div className='flex gap-4 w-full ml-4 mr-24 justify-end md:mr-16'>
-            <div className='flex gap-4 md:text-xl'>
-            <NavLink to='/create'><h1>Create</h1></NavLink>
-            <NavLink to='/login'><button>Login</button></NavLink>
-            <NavLink to='/signup'><button>Signup</button></NavLink>
-            </div>
-        </div>
-    </div>
-  )
-}
+    const [isMenuOpen, setMenuOpen] = useState(false);
 
-export default Navbar
+    return (
+        <nav className="p-4">
+            <div className="flex justify-between items-center">
+                <div className="flex items-center">
+                    <NavLink to="/home">
+                        <img
+                            src={Logo}
+                            className="h-10 w-10 rounded-full"
+                            alt="Logo"
+                        />
+                    </NavLink>
+                    <div className="hidden md:flex ml-4 space-x-4 text-white">
+                        <NavLink to="/tech">Tech</NavLink>
+                        <NavLink to="/business">Business</NavLink>
+                        <NavLink to="/sports">Sports</NavLink>
+                        <NavLink to="/about">About</NavLink>
+                    </div>
+                </div>
+                <div className="hidden md:flex space-x-4 text-white">
+                    <NavLink to="/create">Create</NavLink>
+                    <NavLink to="/login">Login</NavLink>
+                    <NavLink to="/signup">Signup</NavLink>
+                </div>
+                <div className="md:hidden">
+                    <NavLink to="/create" className="mx-2">
+                        Create
+                    </NavLink>
+                    <button
+                        className="fa-solid fa-bars mx-2 fa-2xl"
+                        onClick={() => setMenuOpen(!isMenuOpen)}
+                    ></button>
+                </div>
+            </div>
+            {isMenuOpen && (
+                <div className="md:hidden text-white text-center p-4">
+                    <NavLink to="/tech" className="block my-2 mx-2">
+                        Tech
+                    </NavLink>
+                    <NavLink to="/business" className="block my-2 mx-2">
+                        Business
+                    </NavLink>
+                    <NavLink to="/sports" className="block my-2 mx-2">
+                        Sports
+                    </NavLink>
+                    <NavLink to="/about" className="block my-2 mx-2">
+                        About
+                    </NavLink>
+                    <NavLink to="/login" className="block my-2 mx-2">
+                        Login
+                    </NavLink>
+                    <NavLink to="/signup" className="block my-2 mx-2">
+                        Signup
+                    </NavLink>
+                </div>
+            )}
+        </nav>
+    );
+};
+
+export default Navbar;
