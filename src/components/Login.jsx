07c5022525034d5bc1830e7643dfbd4460c1285d
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2023-07-22 21:56:24 +0530

    Added login and signup components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate, NavLink } from "react-router-dom";
 import { Button } from "./containers/Button";
 
 const Login = () => {
+
+  const navigate=useNavigate();
+    const [Auth, setAuth] = useState({
+        "email":"",
+        "password":""
+    })
+
+    const handleChange=(e)=>{
+        const {name,value}=e.target;
+        setAuth((prevVal)=>{
+            return({
+                ...prevVal,
+                [name]:value
+            })
+        })
+    }
+
+    
+    const handleClick= async (e)=>{
+        e.preventDefault();
+        const response = await fetch("http://localhost:5000/api/auth/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({email:Auth.email,password:Auth.password})
+          });
+          const Data=await response.json();
+          if(Data.success){
+            localStorage.setItem("token",Data.token)
+            navigate("/")
+          }else{
+            alert("Invalid Credentials")
+          }
+        }
+
+
   return (
     <div className="flex justify-center items-start h-screen ">
       <div className="bg-[#343432] p-8 rounded-lg shadow-lg w-auto mt-8 mx-10">
@@ -12,6 +50,7 @@ const Login = () => {
               type="email"
               name="email"
               placeholder="Email"
+              onChange={handleChange}
             ></input>
           </div>
           <div className="mr-1 ml-1 mt-4 ">
@@ -20,10 +59,11 @@ const Login = () => {
               type="password"
               name="password"
               placeholder="Password"
+              onChange={handleChange}
             ></input>
           </div>
           <div className="mt-4 text-center">
-            <Button>Login</Button>
+            <Button onClick={handleClick}>Login</Button>
           </div>
         </form>
       </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,41 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Button } from "./containers/Button";
 
 const Signup = () => {
-  return (
-    <div className="flex justify-center items-start h-screen ">
-      <div className="bg-[#343432] p-4 rounded-lg shadow-lg w-auto mt-8 mx-10">
-        <form>
-          <div className="mr-1 ml-1 mt-4 ">
-            <input
-              className=" w-52 h-10 text-center rounded-t-md border-2 rounded-b-md bg-transparent rounded-sm"
-              name="name"
-              placeholder="Name"
-            ></input>
-          </div>
-          <div className="mr-1 ml-1 mt-4 ">
-            <input
-              className=" w-52 h-10 text-center rounded-t-md border-2 rounded-b-md bg-transparent rounded-sm"
-              type="email"
-              name="email"
-              placeholder="email"
-            ></input>
-          </div>
-          <div className="mr-1 ml-1 mt-4 ">
-            <input
-              className=" w-52 h-10 text-center rounded-t-md border-2 rounded-b-md bg-transparent rounded-sm"
-              type="password"
-              name="password"
-              placeholder="Password"
-            ></input>
-          </div>
-          <div className="mt-4 text-center">
-            <Button>Signup</Button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+    const navigate = useNavigate();
+    const [newAuth, setnewAuth] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setnewAuth((prevVal) => {
+            return {
+                ...prevVal,
+                [name]: value,
+            };
+        });
+    };
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        const response = await fetch(
+            "http://localhost:5000/api/auth/createUser",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: newAuth.name,
+                    email: newAuth.email,
+                    password: newAuth.password,
+                }),
+            }
+        );
+        const Data = await response.json();
+        if (Data.success) {
+            localStorage.setItem("token", Data.token);
+            navigate("/");
+        } else {
+            alert(Data.Error);
+        }
+    };
+
+    return (
+        <div className="flex justify-center items-start h-screen ">
+            <div className="bg-[#343432] p-4 rounded-lg shadow-lg w-auto mt-8 mx-10">
+                <form>
+                    <div className="mr-1 ml-1 mt-4 ">
+                        <input
+                            className=" w-52 h-10 text-center rounded-t-md border-2 rounded-b-md bg-transparent rounded-sm"
+                            name="name"
+                            placeholder="Name"
+                            onChange={handleChange}
+                        ></input>
+                    </div>
+                    <div className="mr-1 ml-1 mt-4 ">
+                        <input
+                            className=" w-52 h-10 text-center rounded-t-md border-2 rounded-b-md bg-transparent rounded-sm"
+                            type="email"
+                            name="email"
+                            placeholder="email"
+                            onChange={handleChange}
+                        ></input>
+                    </div>
+                    <div className="mr-1 ml-1 mt-4 ">
+                        <input
+                            className=" w-52 h-10 text-center rounded-t-md border-2 rounded-b-md bg-transparent rounded-sm"
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={handleChange}
+                        ></input>
+                    </div>
+                    <div className="mt-4 text-center">
+                        <Button onClick={handleClick}>Signup</Button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Signup;
