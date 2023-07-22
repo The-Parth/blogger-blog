0ceeac4af26ab624da4d63df0959c216cdafcdb7
atharvaupare <atharvaupare5@gmail.com>
2023-07-22 18:26:14 +0530

    cards fixed

--- src/components/containers/Carditem.jsx
+++ src/components/containers/Carditem.jsx
@@ -1,15 +1,13 @@
-import React from "react"
-
-
+import React from "react";
 
 const Carditem = ({ Url, content, alt, title }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-[0_35px_60px_-15px_rgba(255,255,255,0.3)] m-4">
+    <div className="max-w-sm rounded overflow-hidden shadow-[0_35px_60px_-15px_rgba(255,255,255,0.3)] m-4 h-[400px] ">
       <img className="w-full" src={Url} alt={alt} />
 
-      <div class="px-6 py-4  bg-[#031130]">
+      <div class="px-6 py-4h-10  bg-[#031130] h-[100%]">
         <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-[#7ea4f7] text-base">{content}</p>
+        <p className="text-[#7ea4f7] text-base  ">{content}</p>
       </div>
     </div>
   );
