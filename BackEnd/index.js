3fd46c27924ce918a5e1d49da81264356bd08b27
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2023-07-23 03:50:48 +0530

    Index.js finale

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,28 +1,19 @@
-const express=require('express');
-const app=express();
-const cors=require("cors");
-const connectToMongo =require("./db")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connectToMongo = require("./db");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json({ limit: "1mb", extended: true }));
+app.use(express.urlencoded({ limit: "1mb", extended: true }));
 
-var bodyParser = require('body-parser');
-// app.use(bodyParser.json({limit: "1mb", extended: true, parameterLimit:50000}));
-// app.use(bodyParser.urlencoded({limit: "1mb", extended: true, parameterLimit:50000}));
-app.use(bodyParser());
-
-app.use(bodyParser.json({ limit: '1mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
-
-
-const Port=5000;
+const Port = 5000;
 
 connectToMongo();
 
-app.use('/api/auth', require('./Routes/auth.js'))
-app.use('/api/blogs', require('./Routes/blogs.js'))
+app.use("/api/auth", require("./Routes/auth.js"));
+app.use("/api/blogs", require("./Routes/blogs.js"));
 
-
-app.listen(Port,()=>{
-    console.log('Server Started')
-})
+app.listen(Port, () => {
+  console.log("Server Started");
+});
