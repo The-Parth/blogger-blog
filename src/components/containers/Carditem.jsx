ac374220ba4a60354fd641d726176ad5996e2621
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2023-07-23 04:16:57 +0530

    Carditem

--- src/components/containers/Carditem.jsx
+++ src/components/containers/Carditem.jsx
@@ -2,17 +2,23 @@ import React from "react";
 import LikeButton from "./LikeButton";
 
 const Carditem = ({ Url, content, alt, title }) => {
-  return (
-    <div className="max-w-sm rounded overflow-hidden shadow-[0_35px_60px_-15px_rgba(255,255,255,0.3)] m-4 ">
-      <img className="w-full" src={Url} alt={alt} />
+    return (
+        <div className="w-full max-w-sm rounded overflow-hidden shadow-[0_35px_60px_-15px_rgba(255,255,255,0.3)] m-4">
+            <div className="w-full aspect-[1.33]">
+                <img
+                    className="w-full object-cover h-full"
+                    src={Url}
+                    alt={alt}
+                />
+            </div>
 
-      <div class="px-6 py-4h-10  bg-[#031130] h-[100%]">
-        <div className="font-bold text-xl mb-2 pt-4">{title}</div>
-        <p className="text-[#7ea4f7] text-base pb-4 ">{content}</p>
-        <LikeButton/>
-      </div>
-    </div>
-  );
+            <div className="px-6 py-4 bg-[#031130] h-[100%] w-full">
+                <div className="font-bold text-xl mb-2 pt-4">{title}</div>
+                <p className="text-[#7ea4f7] text-base pb-4">{content}</p>
+                <LikeButton />
+            </div>
+        </div>
+    );
 };
 
 export default Carditem;
