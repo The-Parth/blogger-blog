0d322561219ffa760f3131ca24c0d8ad7105622f
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2023-07-22 16:04:24 +0530

    prettier

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 const PageNotFound = () => {
     let pages = [
         { name: "the Home page", href: "/" },
@@ -19,7 +18,10 @@ const PageNotFound = () => {
                     <h1 className="text-9xl mt-4">
                         <button
                             className="ml-1 transition ease-in-out text-[#7ea4f7] hover:text-[#6bd896] hover:-translate-y-1 hover:scale-105 duration:300"
-                            onClick={() => window.location.href = "https://en.wikipedia.org/wiki/HTTP_404"}
+                            onClick={() =>
+                                (window.location.href =
+                                    "https://en.wikipedia.org/wiki/HTTP_404")
+                            }
                         >
                             404
                         </button>
@@ -27,10 +29,12 @@ const PageNotFound = () => {
                     <br />
                     <h2 className="text-3xl">Page Not Found</h2>
                     <h2 className="text-2xl mt-4">
-                        In the meantime, try visiting {" "}
+                        In the meantime, try visiting{" "}
                         <button
                             className="ml-1 transition ease-in-out text-[#7ea4f7] hover:text-[#6bd896] hover:-translate-y-1 hover:scale-105 duration:300"
-                            onClick={() => window.location.href = randomPage.href}
+                            onClick={() =>
+                                (window.location.href = randomPage.href)
+                            }
                         >
                             {randomPage.name}
                         </button>
