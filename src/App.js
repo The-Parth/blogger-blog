dde3e131d7fbdf0d7c1b316d9b86b6b32227bee0
atharvaupare <atharvaupare5@gmail.com>
2023-07-23 03:26:18 +0530

    Added footer and updated navbar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import About from './pages/About';
-import Business from './pages/Business';
-import Home from './pages/Home';
-import { Routes, BrowserRouter, Route } from "react-router-dom"
-import Sports from './pages/Sports';
-import Tech from './pages/Tech';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import AddBlog from './pages/AddBlog';
-import BlogState from './context/blog/BlogState';
-import PageNotFound from './pages/404';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Business from "./pages/Business";
+import Home from "./pages/Home";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import Sports from "./pages/Sports";
+import Tech from "./pages/Tech";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import AddBlog from "./pages/AddBlog";
+import BlogState from "./context/blog/BlogState";
+import PageNotFound from "./pages/404";
+import { Footer } from "./components/containers/Footer";
 
 function App() {
   return (
-    <div>
-      <BlogState>
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/home' element={<Home />}></Route>
-            <Route path='/about' element={<About />}></Route>
-            <Route path='/tech' element={<Tech />}></Route>
-            <Route path='/business' element={<Business />}></Route>
-            <Route path='/sports' element={<Sports />}></Route>
-            <Route path='/login' element={<Login />}></Route>
-            <Route path='/signup' element={<Signup />}></Route>
-            <Route path='/create' element={<AddBlog />}></Route>
-            <Route path='*' element={<PageNotFound />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </BlogState>
-    </div>
+    <>
+      <div>
+        <BlogState>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/home" element={<Home />}></Route>
+              <Route path="/about" element={<About />}></Route>
+              <Route path="/tech" element={<Tech />}></Route>
+              <Route path="/business" element={<Business />}></Route>
+              <Route path="/sports" element={<Sports />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/signup" element={<Signup />}></Route>
+              <Route path="/create" element={<AddBlog />}></Route>
+              <Route path="*" element={<PageNotFound />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </BlogState>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/containers/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../../App.css";
+
+export const Footer = () => {
+  return (
+    <footer className=" flex bg-white  shadow dark:bg-[#343432]  fixed bottom-0 w-full">
+      <div className="w-full max-w-screen-xl mx-auto p-2 md:py-4  ">
+        <div className="sm:flex sm:items-center sm:justify-center">
+          <a
+            href="https://www.linkedin.com/company/csi---kjsce-student's-chapter/"
+            className="flex items-center mb-4 sm:mb-0"
+          >
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Made with
+              <i class="fa-solid fa-heart fa-lg text-red-600 mx-1"></i>
+              by Tech Team, CSI-KJSCE.
+            </span>
+          </a>
+          <figure className=" mix-blend-multiply ">
+            <img
+              src={require("../../assets/Csi-logo.png")}
+              alt="CSI-KJSCE logo "
+              className="h-12 mx-3 brightness-0 invert-0 "
+            />
+          </figure>
+        </div>
+        <hr className="my-2 border-gray-200 sm:mx-auto dark:border-gray-700 " />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2023 . No Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+};
