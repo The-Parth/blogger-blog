282a005de1e782a242e9149d96f331cfa32b85f3
atharvaupare <atharvaupare5@gmail.com>
2023-07-23 17:16:34 +0530

    basic validation

--- src/pages/AddBlog.jsx
+++ src/pages/AddBlog.jsx
@@ -4,202 +4,232 @@ import BlogContext from "../context/blog/blogcontext";
 import { useNavigate } from "react-router-dom";
 
 const AddBlog = () => {
-    const formRef = useRef(null);
-    const [selectedImage, setSelectedImage] = useState(null);
+  const formRef = useRef(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [formErrors, setFormErrors] = useState({
+    title: false,
+    description: false,
+    post: false,
+  });
 
-    const handleFileChange = (event) => {
-        const selectedFile = event.target.files[0];
-        setSelectedImage(selectedFile);
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setSelectedImage(selectedFile);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formValid = validateForm();
+    if (!formValid) {
+      return;
+    }
+
+    // Rest of the code for handling form submission...
+
+    // Get form data from the refs
+    const formData = {
+      title: formRef.current.title.value,
+      description: formRef.current.description.value,
+      post: formRef.current.post.value,
+      category: formRef.current.category.value,
+      image: selectedImage,
     };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    //console.log(formData);
+    formRef.current.title.value = "";
+    formRef.current.description.value = "";
+    formRef.current.post.value = "";
+    formRef.current.category.value = "";
+    setSelectedImage(null);
+    handleClick(event, formData.image);
+    navigate("/");
+  };
+  const navigate = useNavigate();
+  const { addBlogs } = useContext(BlogContext);
+  const [newBlog, setNewBlog] = useState({
+    title: "",
+    description: "",
+    post: "",
+    category: "business",
+    image: "",
+  });
 
-        // Get form data from the refs
-        const formData = {
-            title: formRef.current.title.value,
-            description: formRef.current.description.value,
-            post: formRef.current.post.value,
-            category: formRef.current.category.value,
-            image: selectedImage,
-        };
-
-        //console.log(formData);
-        formRef.current.title.value = "";
-        formRef.current.description.value = "";
-        formRef.current.post.value = "";
-        formRef.current.category.value = "";
-        setSelectedImage(null);
-        handleClick(event, formData.image);
-    };
-    const navigate = useNavigate();
-    const { addBlogs } = useContext(BlogContext);
-    const [newBlog, setNewBlog] = useState({
-        title: "",
-        description: "",
-        post: "",
-        category: "business",
-        image: "",
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewBlog((prevValues) => {
+      return {
+        ...prevValues,
+        [name]: value,
+      };
     });
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setNewBlog((prevValues) => {
-            return {
-                ...prevValues,
-                [name]: value,
-            };
-        });
+  };
+  const validateForm = () => {
+    // Basic validation for required fields
+    const errors = {
+      title: newBlog.title.trim() === "",
+      description: newBlog.description.trim() === "",
+      post: newBlog.post.trim() === "",
     };
 
-    const handleClick = (e, img) => {
-        function imageFileToBase64(file) {
-            return new Promise((resolve, reject) => {
-                const reader = new FileReader();
-                reader.onloadend = () => {
-                    resolve(reader.result);
-                };
-                reader.onerror = reject;
-                reader.readAsDataURL(file);
-            });
+    setFormErrors(errors);
+
+    // Check if any required field is empty
+    return !Object.values(errors).some((error) => error);
+  };
+
+  const handleClick = (e, img) => {
+    function imageFileToBase64(file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          resolve(reader.result);
+        };
+        reader.onerror = reject;
+        reader.readAsDataURL(file);
+      });
+    }
+
+    // Usage example:
+    async function getB64(myFile) {
+      try {
+        const file = myFile;
+        //console.log(file);
+        if (!file) {
+          return "";
         }
 
-        // Usage example:
-        async function getB64(myFile) {
-            try {
-                const file = myFile;
-                //console.log(file);
-                if (!file) {
-                    return "";
-                }
+        const base64String = await imageFileToBase64(file);
+        return base64String;
+      } catch (error) {
+        console.error("Error converting image to Base64:", error);
+        return "";
+      }
+    }
 
-                const base64String = await imageFileToBase64(file);
-                return base64String;
-            } catch (error) {
-                console.error("Error converting image to Base64:", error);
-                return "";
-            }
-        }
+    e.preventDefault();
+    getB64(img).then((data) => {
+      newBlog.image = data;
+      console.log(newBlog);
+      addBlogs(
+        newBlog.title,
+        newBlog.description,
+        newBlog.post,
+        newBlog.category,
+        newBlog.image
+      );
+      navigate("/");
+    });
+  };
 
-        e.preventDefault();
-        getB64(img).then((data) => {
-            newBlog.image = data;
-            console.log(newBlog);
-            addBlogs(
-                newBlog.title,
-                newBlog.description,
-                newBlog.post,
-                newBlog.category,
-                newBlog.image
-            );
-            navigate("/");
-        });
-    };
-
-    return (
-        <div className="container mx-auto p-2">
-            <div className="max-w-sm mx-auto my-24 bg-[#343432] px-5 py-10 rounded shadow-xl">
-                <div className="text-center mb-8">
-                    <h1 className="font-bold text-4xl ">Write it out!</h1>
-                </div>
-                <form ref={formRef} onSubmit={handleClick}>
-                    <div className="mt-5">
-                        <label htmlFor="title">Title</label>
-                        <input
-                            onChange={handleChange}
-                            name="title"
-                            type="text"
-                            id="title"
-                            className="block w-full p-2 border-2 rounded  border-[#7ea4f7] text-[#010101]"
-                        />
-                    </div>
-                    <div className="mt-5">
-                        <label htmlFor="description">Description</label>
-                        <input
-                            onChange={handleChange}
-                            name="description"
-                            type="text"
-                            id="description"
-                            className="block w-full p-2 border-2 rounded border-[#7ea4f7] text-[#010101]"
-                        />
-                    </div>
-                    <div className="mt-5">
-                        <label htmlFor="post">Post</label>
-                        <textarea
-                            onChange={handleChange}
-                            name="post"
-                            id="post"
-                            rows="8"
-                            className="block w-full p-2 border-2 rounded border-[#7ea4f7] text-[#010101] resize-none"
-                        />
-                    </div>
-                    <div className="mt-4 text-[#010101]">
-                        <label htmlFor="category " className="text-white">
-                            Category
-                        </label>
-                        <select
-                            onChange={handleChange}
-                            name="category"
-                            id="category"
-                            className="block w-full p-2 border-2 rounded border-[#7ea4f7] "
-                        >
-                            <option value="business">Business</option>
-                            <option value="sports">Sports</option>
-                            <option value="tech">Tech</option>
-                        </select>
-                    </div>
-                    <div className="mt-8">
-                        {selectedImage ? (
-                            <label
-                                htmlFor="image"
-                                className="py-2 px-3 ml-3 font-bold bg-green-500 rounded text-white cursor-pointer"
-                            >
-                                <i className="fa-solid fa-circle-check fa-xl"></i>
-                            </label>
-                        ) : (
-                            <>
-                                <label htmlFor="image">Add Image</label>
-                                <input
-                                    type="file"
-                                    id="image"
-                                    accept="image/*"
-                                    className="hidden w-full p-2 border rounded border-gray-500"
-                                    onChange={handleFileChange}
-                                />
-
-                                <label
-                                    htmlFor="image"
-                                    className="py-2 px-3 ml-3 font-bold bg-[#7ea4f7] rounded text-white cursor-pointer inline-block group transition ease-in duration-300 hover:bg-[#5873ad]"
-                                >
-                                    <i className="fa-solid fa-plus fa-lg bg-[#7ea4f7] rounded text-[#222] group-hover:text-white transition ease-in duration-300"></i>
-                                </label>
-                            </>
-                        )}
-                    </div>
-
-                    {selectedImage && (
-                        <div className="mt-4">
-                            <img
-                                src={URL.createObjectURL(selectedImage)}
-                                alt="Selected"
-                                className="max-w-full h-auto rounded"
-                            />
-                        </div>
-                    )}
-                    <div className="mt-10 font-bold  ">
-                        <button
-                            onClick={handleSubmit}
-                            type="submit"
-                            value="Submit"
-                            className="py-3 bg-[#7ea4f7] hover:bg-[#5873ad] hover:text-white rounded text-[#222]   text-center w-full ease-in duration-300 "
-                        >
-                            Submit
-                        </button>
-                    </div>
-                </form>
-            </div>
+  return (
+    <div className="container mx-auto p-2">
+      <div className="max-w-sm mx-auto my-24 bg-[#343432] px-5 py-10 rounded shadow-xl">
+        <div className="text-center mb-8">
+          <h1 className="font-bold text-4xl ">Write it out!</h1>
         </div>
-    );
+        <form ref={formRef} onSubmit={handleClick}>
+          <div className="mt-5">
+            <label htmlFor="title">Title</label>
+            <input
+              onChange={handleChange}
+              name="title"
+              type="text"
+              id="title"
+              className="block w-full p-2 border-2 rounded  border-[#7ea4f7] text-[#010101]"
+              required
+            />
+          </div>
+          <div className="mt-5">
+            <label htmlFor="description">Description</label>
+            <input
+              onChange={handleChange}
+              name="description"
+              type="text"
+              id="description"
+              className="block w-full p-2 border-2 rounded border-[#7ea4f7] text-[#010101]"
+              required
+            />
+          </div>
+          <div className="mt-5">
+            <label htmlFor="post">Post</label>
+            <textarea
+              onChange={handleChange}
+              name="post"
+              id="post"
+              rows="8"
+              className="block w-full p-2 border-2 rounded border-[#7ea4f7] text-[#010101] resize-none"
+              required
+            />
+          </div>
+          <div className="mt-4 text-[#010101]">
+            <label htmlFor="category " className="text-white">
+              Category
+            </label>
+            <select
+              onChange={handleChange}
+              name="category"
+              id="category"
+              className="block w-full p-2 border-2 rounded border-[#7ea4f7] "
+              required
+            >
+              <option value="business">Business</option>
+              <option value="sports">Sports</option>
+              <option value="tech">Tech</option>
+            </select>
+          </div>
+          <div className="mt-8">
+            {selectedImage ? (
+              <label
+                htmlFor="image"
+                className="py-2 px-3 ml-3 font-bold bg-green-500 rounded text-white cursor-pointer"
+              >
+                <i className="fa-solid fa-circle-check fa-xl"></i>
+              </label>
+            ) : (
+              <>
+                <label htmlFor="image">Add Image</label>
+                <input
+                  type="file"
+                  id="image"
+                  accept="image/*"
+                  className="hidden w-full p-2 border rounded border-gray-500"
+                  onChange={handleFileChange}
+                  required
+                />
+
+                <label
+                  htmlFor="image"
+                  className="py-2 px-3 ml-3 font-bold bg-[#7ea4f7] rounded text-white cursor-pointer inline-block group transition ease-in duration-300 hover:bg-[#5873ad]"
+                >
+                  <i className="fa-solid fa-plus fa-lg bg-[#7ea4f7] rounded text-[#222] group-hover:text-white transition ease-in duration-300"></i>
+                </label>
+              </>
+            )}
+          </div>
+
+          {selectedImage && (
+            <div className="mt-4">
+              <img
+                src={URL.createObjectURL(selectedImage)}
+                alt="Selected"
+                className="max-w-full h-auto rounded"
+              />
+            </div>
+          )}
+          <div className="mt-10 font-bold  ">
+            <button
+              onClick={handleSubmit}
+              type="submit"
+              value="Submit"
+              className="py-3 bg-[#7ea4f7] hover:bg-[#5873ad] hover:text-white rounded text-[#222]   text-center w-full ease-in duration-300 "
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddBlog;
