17da05c4084e85fb2380ff438ec3d2ad7f47dfe4
atharvaupare <atharvaupare5@gmail.com>
2023-07-23 14:58:18 +0530

    description

--- src/components/containers/Card.jsx
+++ src/components/containers/Card.jsx
@@ -9,7 +9,10 @@ const Card = () => {
     <div className="flex justify-center">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {blog.map((n, index) => (
-          <div key={index} className="flex items-stretch ">
+          <div
+            key={index}
+            className="flex items-stretch transition-transform transform hover:scale-105"
+          >
             <Carditem
               newid={n._id}
               Url={
@@ -18,6 +21,7 @@ const Card = () => {
                   : "https://images.theconversation.com/files/182925/original/file-20170822-30538-gebk45.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop"
               }
               title={n.title}
+              description={n.description}
               content={n.post}
               user={n.user}
               id={n._id}
--- src/components/containers/Carditem.jsx
+++ src/components/containers/Carditem.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import LikeButton from "./LikeButton";
 import { useNavigate } from "react-router-dom";
 
-const Carditem = ({ Url, content, alt, title, user,newid }) => {
-    const host = "http://localhost:5000";
-    const [userobj, setUserObj] = useState(null);
-    const navigate=useNavigate();
+const Carditem = ({ Url, content, alt, title, description, user, newid }) => {
+  const host = "http://localhost:5000";
+  const [userobj, setUserObj] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getUsername = async (user) => {
@@ -28,26 +28,33 @@ const Carditem = ({ Url, content, alt, title, user,newid }) => {
     getUsername(user);
   }, [user]);
 
-    if (!userobj) {
-        // Return some loading UI or null while waiting for the user data
-        return null;
-    }
-    const handleBig=(passid)=>{
-        navigate(`/blog/${passid}`)
-    }
+  if (!userobj) {
+    // Return some loading UI or null while waiting for the user data
+    return null;
+  }
+  const handleBig = (passid) => {
+    navigate(`/blog/${passid}`);
+  };
 
-    return (
-      <div className="w-full max-w-sm rounded overflow-hidden shadow-[0_35px_60px_-15px_rgba(255,255,255,0.3)] m-4 cursor-pointer" onClick={()=>{handleBig(newid)}}>
+  return (
+    <div className="w-full max-w-sm rounded overflow-hidden shadow-[0_35px_60px_-15px_rgba(255,255,255,0.3)] m-4 cursor-pointer">
       <div className="w-full aspect-[1.33] relative group">
         <img className="w-full object-cover h-full" src={Url} alt={alt} />
-        <div className="flex items-end justify-end absolute bottom-0 right-0 w-full h-full bg-gradient-to-b from-transparent to-gray-700 opacity-0 group-hover:opacity-100 transition-opacity">
-          <LikeButton title={title} id={id} />
+        <div className="flex items-end justify-end absolute bottom-0 right-0 w-full h-full bg-gradient-to-b from-transparent to-gray-700 opacity-0 group-hover:opacity-100 transition-opacity z-9">
+          <LikeButton title={title} />
         </div>
       </div>
 
-      <div className="px-6 py-4 bg-[#031130] h-[100%] w-full">
+      <div
+        className="px-6 py-4 bg-[#031130] h-[100%] w-full"
+        onClick={() => {
+          handleBig(newid);
+        }}
+      >
         <div className="font-bold text-xl mb-2 pt-4 break-words">{title}</div>
-        <p className="text-[#7ea4f7] text-base pb-4 break-words">{content}</p>
+        <p className="text-[#7ea4f7] text-base pb-4 break-words">
+          {description}
+        </p>
         <p className="text-gray-400 text-xs">- by {userobj.name}</p>
       </div>
     </div>
